f51f386ace322afe1b4c61740038b58eae03d8be
unknown <nouranmalidi@gmail.com>
2026-01-29 16:34:17 +0300

    creation des api route

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
 // Ici, nous créons nottre application EpressJS
 
 // J'importe le framework ExpressJS
+const console = require('console');
 const express = require ('express');
+
+
 
 // J'initialise une application express
 const app = express();
 
 
+// API ROUTE pour la racine 
+app.get('/', (req,res) => {   // Pas de next car c'est des route et pas des middleware
+    res.write("<h1> Bienvenue chey May Gourmet </h1>");
+    res.end();
+});
+
+
+// API ROUTE pour la page d'accueil
+app.get('/api/accueil', (req, res) => {
+  console.log("Je passe dans /api/accueil");
+
+// Le type d'encodage du texte retourné en réponse
+res.writeHead(200, { "content-type": "text/html;charset=utf-8"});
+
+// Le contenu qui sera affiché côté navigateur web
+res.write("<p> Je suis à l'accueil<p>");
+
+// Fin de la réponse
+res.end();
+
+});
 
 module.exports = app;
--- serveur.js
+++ serveur.js
@@ -4,15 +4,20 @@
 
 const http = require('http') // C'est http qui va créer un serveur
 
-// J'importe l'application express app.js
+// J'importe l'application express app.js qui nous permet de créer les routes
 const app = require('./app');
 
-
+// Stockage du port en dur pour éviter la saisie en dur
 const numeroPort = 3004;
 
+// Je configure le numéro de port utiliser dans le serveur
+app.set('port',numeroPort);
+
+
+// Création du serveur en utilisant express (app) en paramètre de la fonction createServer
 const serveur = http.createServer(app);
 
 serveur.listen(numeroPort, ()=> {
-    console.log("Le serveur de MayGourmet est à l'écoute sur le port", numeroPort)
-})
+    console.log("Le serveur de MayGourmet est à l'écoute sur le port", numeroPort);
+});
 
